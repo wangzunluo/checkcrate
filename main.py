0785695a62cbc91671791867805c590202510f98
unknown <wangarwin13@gmail.com>
2023-03-09 20:58:11 -0600

    write functions for code

--- main.py
+++ main.py
@@ -7,50 +7,76 @@ urllib3.disable_warnings()
 import json
 import time
 
+
+filename = 'C:\Riot Games\League of Legends\lockfile'
+username = 'riot'
+account_session = None
+base_url = None
+data = None
+
 def poll():
     start_time = get_file_time()
     while(True):
         file_time = get_file_time()
         if start_time != file_time:
-            update_chests()
+            update_data_file()
             start_time = file_time
         time.sleep(60)
     
 def get_file_time():
-    filename = 'C:\Riot Games\League of Legends\lockfile'
+    global filename
     return os.stat(filename).st_mtime
 
-def update_chests():
-    data = get_existing_data()
-    chest_data = get_chest()
+def update_data_file():
+    global data
+    name = get_name()
+    data[name] = get_chest_info()
+    save_data()
 
+def read_lock():
+    global filename
+    file = open(filename)
+    fcontents = file.readline()
+    file.close()
+    print(fcontents)
+    return fcontents
 
-file = open(filename)
-fcontents = file.readline()
-file.close()
-print(fcontents)
+def parse_lock():
+    global base_url
+    global account_session
+    lock_content = read_lock().split(':')
+    port = lock_content[2]
+    password = lock_content[3]
+    format = lock_content[4]
+    base_url = format+'://127.0.0.1:'+port
+    account_session = requests.Session()
+    account_session.auth = (username, password)
 
-fcontents = fcontents.split(':')
+def get_name():
+    global account_session
+    response = account_session.get(base_url+'/lol-summoner/v1/current-summoner', verify=False)
+    response = response.json()
+    return response['displayName']
 
-port = fcontents[2]
-username = 'riot'
-password = fcontents[3]
-format = fcontents[4]
+def get_chest_info():
+    global account_session
+    response = account_session.get(base_url+'/lol-collections/v1/inventories/chest-eligibility', verify=False)
+    return response.json()
 
-base_url = format+'://127.0.0.1:'+port
-account_session = requests.Session()
-account_session.auth = ('riot', password)
+def get_data():
+    global data
+    datafile = open('data.json', 'r')
+    data = json.load(datafile)
+    datafile.close()
 
-response = account_session.get(base_url+'/lol-collections/v1/inventories/chest-eligibility', verify=False)
-chest_info = response.json()
+def save_data():
+    global data
+    datafile = open('data.json', 'w')
+    datafile.write(json.dumps(data))
+    datafile.close()
 
-datafile = open('data.json', 'r+')
+def main():
+    get_data()
 
-data = json.load(datafile)
-
-data['wzl7'] = 'test'
-
-datafile.seek(0)
-datafile.write(json.dumps(data))
-datafile.truncate()
-
+if __name__ == '__main__':
+    main()
